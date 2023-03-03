55b891a5f68ad9f990e21b4ad9d0c8b64a171ea3
jswan <jswannieman@gmail.com>
2023-03-04 10:39:34 +1100

    Update directory Javascript Component

--- File-Handling/directory.js
+++ File-Handling/directory.js
@@ -1,19 +1,59 @@
 const fs = require("fs");
+const path = require("path");
 
-if (!fs.existsSync("../directory")) {
+const businessData =
+  "\nWe need to pay close attention to this matter, in order to succeed the bussiness.";
+
+if (!fs.existsSync("./directory")) {
   fs.mkdir("./directory", (error) => {
     if (error) {
       throw error;
+    } else {
+      fs.mkdir("./directory/sub-directory", (error) => {
+        if (error) {
+          throw error;
+        }
+      });
     }
-    console.log("Directory Created");
+    console.log(`Directory and Sub-Directory Created`);
   });
 }
 
-if (fs.existsSync("./directory")) {
-  fs.rmdir("../directory", (error) => {
+if (fs.existsSync("./directory/sub-directory")) {
+  fs.appendFile(
+    path.join(__dirname, "directory/sub-directory", "business-file.txt"),
+    businessData,
+    "utf8",
+    (error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log(`File "business-file.txt" Written`);
+      }
+    }
+  );
+}
+
+if (!fs.existsSync("./directory2")) {
+  fs.mkdir("./directory2", (error) => {
+    if (error) {
+      throw error;
+    } else {
+      fs.mkdir("./directory2/sub-directory2", (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+    }
+    console.log(`Directory2 and Sub-Directory2 Created`);
+  });
+}
+
+if (fs.existsSync("./directory2/sub-directory2")) {
+  fs.rmdir("./directory2/sub-directory2", (error) => {
     if (error) {
       throw error;
     }
-    console.log("Directory Removed");
+    console.log("sub-directory2 Removed");
   });
 }
