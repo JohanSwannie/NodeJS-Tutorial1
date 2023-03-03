c41ce5c5afa1259c9d2834efe377ff3dcf2a74bb
jswan <jswannieman@gmail.com>
2023-03-03 12:31:03 +1100

    Place Files under new folders

---
+++ File-Handling/directory.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+if (!fs.existsSync("../directory")) {
+  fs.mkdir("./directory", (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log("Directory Created");
+  });
+}
+
+if (fs.existsSync("./directory")) {
+  fs.rmdir("../directory", (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log("Directory Removed");
+  });
+}
---
+++ File-Handling/handle-files2.js
@@ -0,0 +1,31 @@
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+// Asynchronous File Operations
+
+const fileAsyncRead = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "Files", "asyncfile1.txt"),
+      "utf8"
+    );
+    console.log(data);
+    await fsPromises.writeFile(
+      path.join(__dirname, "Files", "asyncfile1b.txt"),
+      data
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, "Files", "asyncfile1b.txt"),
+      "\n\nFile has been read and appended asynchronously"
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "Files", "asyncfile1b.txt"),
+      path.join(__dirname, "Files", "asyncfile2.txt")
+    );
+    await fsPromises.unlink(path.join(__dirname, "Files", "dummyfile.txt"));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fileAsyncRead();
