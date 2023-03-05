e4ffe7bac49fd3a208675f43f89007d091f1dc04
jswan <jswannieman@gmail.com>
2023-03-06 08:36:38 +1100

    Update server javascript component

--- Node-Server/server.js
+++ Node-Server/server.js
@@ -1,11 +1,11 @@
 // node.js runs on the server and not in a browser(backend)
 // The console is the terminal window
 
-// console.log("Hallo jou grapjas");
+console.log("Dummy message to test the console");
 
 // Global object instead of window object
 
-// console.log(global);
+console.log(global);
 
 // Has common core modules
 // Common JS Modules instead of ES6 Modules
@@ -22,12 +22,12 @@ console.log(
   )} | 15 times 8 = ${multiply(15, 8)} | 81 divide by 9 =  ${divide(81, 9)}`
 );
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
-// console.log(path.parse(__filename));
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log(__dirname);
+console.log(__filename);
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
